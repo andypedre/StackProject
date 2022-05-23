bc05e842844892076a0a67ae35c136529606fbb1
JD <79108823+JMDProgrammer@users.noreply.github.com>
2022-05-23 17:58:44 -0400

    Update Main.java
    added a few lines for testing purposes

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,33 @@ public class Main {
 
         //testing class connection to interface within the main class
         System.out.println(testClass(testStack.a));
+
+        //testing colors
+        for(int i =0; 10 >= i ; i++){
+
+            if(i==5) {
+                System.out.println("blue brackets ");
+                System.out.println(".");
+                for(int j =0; 10>= j; j++){
+                    if(j==5 || j==4 ){
+                        System.out.print("yellow brackets ");
+                        }
+                        else {
+                        System.out.println("yellow else brackets");
+                        do {
+                            System.out.println("do it");
+                           break;
+                        } while (i == 5); {
+                            System.out.println("loop again green brackets");
+                            i++;
+                        }
+
+
+                    }
+
+                }
+            }
+        }
     }
 
     public static int testClass(int fun){
